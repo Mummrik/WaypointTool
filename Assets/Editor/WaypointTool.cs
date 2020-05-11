0f24edab19695847b985c904ee5e40d1a21f9088
Mummrik <joakim.lembke@futuregames.nu>
2020-05-11 17:05:25 +0200

    no message

--- Assets/Editor/WaypointTool.cs
+++ Assets/Editor/WaypointTool.cs
@@ -7,21 +7,31 @@ public class WaypointTool : EditorWindow
 {
     [MenuItem("Tools/Waypoint Creator")]
     public static void OpenWaypointTool() => GetWindow<WaypointTool>();
+    [SerializeField] private bool loopWaypoints;
+    [SerializeField] private int waypointIndex;
 
     private bool hasWaypointSystem;
-    private bool loopWaypoints;
-    private bool drawBezier;
 
     private List<Vector3> waypoints;
 
     SerializedObject so;
+    SerializedProperty propLoopWaypoints;
+    SerializedProperty propWaypointIndex;
+
+    private bool drawPreviewWaypointMovement;
+    private Vector3 previewWaypoint;
+    private Vector3 previewTargetWaypoint;
+    private int previewWaypointIndex;
 
     private void OnEnable()
     {
+        so = new SerializedObject(this);
         loopWaypoints = false;
         hasWaypointSystem = false;
         waypoints = new List<Vector3>();
-        so = new SerializedObject(this);
+        propLoopWaypoints = so.FindProperty("loopWaypoints");
+        propWaypointIndex = so.FindProperty("waypointIndex");
+
         Selection.selectionChanged += Repaint;
         SceneView.duringSceneGui += DuringSceneGUI;
     }
@@ -34,46 +44,82 @@ public class WaypointTool : EditorWindow
 
     private void DuringSceneGUI(SceneView sceneView)
     {
+        if (waypoints.Count > 1)
+        {
+            Handles.BeginGUI();
+            if (GUI.Button(new Rect(8, 8, 125, 20), "Preview Movement"))
+            {
+                drawPreviewWaypointMovement = !drawPreviewWaypointMovement;
+                ResetPreview();
+            }
+            if (loopWaypoints == false && drawPreviewWaypointMovement)
+            {
+                if (GUI.Button(new Rect(135, 8, 60, 20), "Restart"))
+                {
+                    ResetPreview();
+                }
+            }
+            Handles.EndGUI();
+        }
+
         if (waypoints.Count > 0)
         {
             for (int i = 0; i < waypoints.Count; i++)
             {
                 Vector3 waypoint = waypoints[i];
+                Handles.Label(waypoint + Vector3.up * 0.2f, $"Waypoint [{i}]");
                 waypoints[i] = Handles.PositionHandle(waypoint, Quaternion.identity);
-                if (i == 0)
+                if (waypoints[i] != waypoint)
                 {
-                    Handles.color = Color.yellow;
-                }else if(i == waypoints.Count - 1)
-                {
-                    Handles.color = Color.green;
+                    //Debug.Log($"{i} moved");
+                    //Undo.RecordObject(waypoint, $"Move {i}");
                 }
-                else
-                {
-                    Handles.color = Color.white;
-                }
+                Handles.color = i == 0 ? Color.yellow : i == waypoints.Count - 1 ? Color.green : Color.white;
                 Handles.SphereHandleCap(i, waypoint, Quaternion.identity, 0.1f, EventType.Repaint);
                 Vector3 waypointNext = loopWaypoints ? i < waypoints.Count - 1 ? waypoints[i + 1] : waypoints[0] : i < waypoints.Count - 1 ? waypoints[i + 1] : waypoints[i];
-                float halfHeight = (waypoint.z - waypointNext.z) * .5f;
-                Vector3 offset = Vector3.forward * (halfHeight < 0 ? halfHeight : -halfHeight);
 
-                if (drawBezier)
+                Handles.color = Color.magenta;
+                Handles.DrawLine(waypoint, waypointNext);
+                Handles.color = Color.white;
+
+                Handles.color = Color.magenta;
+                Vector3 midPoint = (waypoint + waypointNext) * 0.5f;
+                if (!loopWaypoints && i == waypoints.Count - 1)
                 {
-                    Handles.DrawBezier(
-                        waypoint,
-                        waypointNext,
-                        waypoint - offset,
-                        waypointNext + offset,
-                        Color.magenta,
-                        EditorGUIUtility.whiteTexture,
-                        1f
-                    );
+                    midPoint += Vector3.forward * 0.25f;
+                    Handles.Label(midPoint + Vector3.up * 0.1f, $"Add New");
                 }
-                else
+                if (Handles.Button(midPoint, Quaternion.identity, 0.05f, 0.02f, Handles.SphereHandleCap))
                 {
-                    Handles.color = Color.magenta;
-                    Handles.DrawLine(waypoint, waypointNext);
-                    Handles.color = Color.white;
+                    waypoints.Insert(i + 1, midPoint + (i < waypoints.Count - 1 ? Vector3.zero : (Vector3.forward * 0.25f)));
                 }
+            }
+
+            if (drawPreviewWaypointMovement)
+            {
+                if (previewWaypoint == previewTargetWaypoint)
+                {
+                    previewWaypointIndex++;
+                    if (loopWaypoints)
+                    {
+                        if (previewWaypointIndex > waypoints.Count)
+                        {
+                            previewWaypointIndex = 0;
+                        }
+                        previewTargetWaypoint = previewWaypointIndex < waypoints.Count ? waypoints[previewWaypointIndex] : waypoints[0];
+                    }
+                    else
+                    {
+                        if (previewWaypointIndex < waypoints.Count)
+                        {
+                            previewTargetWaypoint = waypoints[previewWaypointIndex];
+                        }
+                    }
+                }
+
+                previewWaypoint = Vector3.MoveTowards(previewWaypoint, previewTargetWaypoint, 0.01f);
+                Handles.color = Color.blue;
+                Handles.SphereHandleCap(-1, previewWaypoint, Quaternion.identity, 0.2f, EventType.Repaint);
             }
         }
         sceneView.Repaint();
@@ -81,19 +127,43 @@ public class WaypointTool : EditorWindow
 
     private void OnGUI()
     {
+        GUILayout.Space(20f);
         so.Update();
-        if (so.ApplyModifiedProperties())
+        if (hasWaypointSystem)
         {
-
+            propLoopWaypoints.boolValue = propLoopWaypoints.boolValue;
+            EditorGUILayout.PropertyField(propLoopWaypoints);
         }
 
-        GUILayout.Space(20f);
+
         if (!hasWaypointSystem)
         {
-            if (GUILayout.Button("Create new waypoint system"))
+            if (GUILayout.Button("Create new"))
             {
                 hasWaypointSystem = true;
+                drawPreviewWaypointMovement = false;
                 waypoints = new List<Vector3>();
+
+                //TODO: Get the sceneView camera center as starting point
+                Vector3 point = Vector3.zero;
+                waypoints.Add(point);
+            }
+            using (new EditorGUI.DisabledScope(Selection.gameObjects.Length != 1))
+            {
+                if (GUILayout.Button("Load from selected"))
+                {
+                    WaypointSystem waypointSystem = Selection.activeGameObject.GetComponent<WaypointSystem>();
+                    if (waypointSystem)
+                    {
+                        hasWaypointSystem = true;
+                        waypoints = waypointSystem.GetWaypoints();
+                        ResetPreview();
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Selection do not have a waypoint system!");
+                    }
+                }
             }
         }
         else
@@ -106,23 +176,94 @@ public class WaypointTool : EditorWindow
                     point = waypoints[waypoints.Count - 1] + (Vector3.forward * 0.5f);
 
                 waypoints.Add(point);
+                ResetPreview();
             }
 
-            using (new EditorGUI.DisabledScope(Selection.gameObjects.Length == 0 || Selection.gameObjects.Length > 1))
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PropertyField(propWaypointIndex);
+            if (GUILayout.Button("Add"))
+            {
+                //TODO: Get the sceneView camera center as starting point
+                Vector3 point = Vector3.zero;
+                if (waypoints.Count > 0 && propWaypointIndex.intValue < waypoints.Count - 1)
+                {
+                    if (propWaypointIndex.intValue < 1)
+                    {
+                        point = (waypoints[propWaypointIndex.intValue] + Vector3.back) * 0.5f;
+                    }
+                    else
+                    {
+                        point = (waypoints[propWaypointIndex.intValue - 1] + waypoints[propWaypointIndex.intValue]) * 0.5f;
+                    }
+                }
+                else
+                    point = waypoints[waypoints.Count - 1] + (Vector3.forward * 0.5f);
+
+                waypoints.Insert(propWaypointIndex.intValue, point);
+                ResetPreview();
+            }
+
+            if (GUILayout.Button("Remove"))
+            {
+                if (propWaypointIndex.intValue < 0 || propWaypointIndex.intValue > waypoints.Count - 1) { return; }
+                waypoints.RemoveAt(propWaypointIndex.intValue);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            GUILayout.Space(100f);
+            using (new EditorGUI.DisabledScope(Selection.gameObjects.Length != 1))
             {
                 if (GUILayout.Button("Apply to selected object"))
                 {
                     GameObject go = Selection.activeObject as GameObject;
-                    if (go.GetComponent<WaypointSystem>())
+                    WaypointSystem waypointSystem = go.GetComponent<WaypointSystem>();
+                    if (!waypointSystem)
                     {
-                        //TODO: Replace the waypoint system data
+                        waypointSystem = go.AddComponent<WaypointSystem>();
+                    }
+
+                    waypointSystem.SetupWaypointSystem(waypoints, propLoopWaypoints.boolValue);
+                }
+            }
+
+            if (GUILayout.Button("Discard"))
+            {
+                ResetPreview();
+                waypoints = new List<Vector3>();
+                hasWaypointSystem = false;
+            }
+            if (Selection.activeGameObject)
+            {
+                if (GUILayout.Button("Load from selected"))
+                {
+                    WaypointSystem waypointSystem = Selection.activeGameObject.GetComponent<WaypointSystem>();
+                    if (waypointSystem)
+                    {
+                        hasWaypointSystem = true;
+                        waypoints = waypointSystem.GetWaypoints();
+                        loopWaypoints = waypointSystem.LoopWaypoints;
+                        ResetPreview();
                     }
                     else
                     {
-                        go.AddComponent<WaypointSystem>();
+                        Debug.LogWarning("Selection do not have a waypoint system!");
                     }
                 }
             }
+
         }
+
+        if (so.ApplyModifiedProperties())
+        {
+            SceneView.RepaintAll();
+            Repaint();
+        }
+    }
+
+    private void ResetPreview()
+    {
+        previewWaypointIndex = 0;
+        previewWaypoint = waypoints[previewWaypointIndex];
+        previewTargetWaypoint = waypoints[previewWaypointIndex];
     }
 }
--- Assets/Scripts/WaypointSystem.cs
+++ Assets/Scripts/WaypointSystem.cs
@@ -5,4 +5,55 @@ using UnityEngine;
 public class WaypointSystem : MonoBehaviour
 {
     //TODO: Add the waypoint data and functions
+    [SerializeField] private List<Vector3> m_Waypoints;
+    [SerializeField] private bool m_LoopWaypoints;
+
+    private Vector3 m_TargetPosition;
+    private int m_WaypointIndex;
+
+    public bool LoopWaypoints { get => m_LoopWaypoints; set => m_LoopWaypoints = value; }
+
+    public List<Vector3> GetWaypoints() => m_Waypoints;
+
+    public void SetupWaypointSystem( in List<Vector3> waypoints, in bool loopWaypoints)
+    {
+        m_Waypoints = waypoints;
+        LoopWaypoints = loopWaypoints;
+        m_Waypoints[0] = gameObject.transform.position;
+        for (int i = 1; i < m_Waypoints.Count; i++)
+        {
+            Vector3 waypoint = m_Waypoints[i];
+            m_Waypoints[i] = waypoint + m_Waypoints[0];
+        }
+        m_TargetPosition = m_Waypoints[m_WaypointIndex];
+    }
+
+
+    private void Update()
+    {
+        if (m_Waypoints != null)
+        {
+            if (transform.position == m_TargetPosition)
+            {
+                m_WaypointIndex++;
+                if (LoopWaypoints)
+                {
+                    if (m_WaypointIndex > m_Waypoints.Count)
+                    {
+                        m_WaypointIndex = 0;
+                    }
+                    m_TargetPosition = m_WaypointIndex < m_Waypoints.Count ? m_Waypoints[m_WaypointIndex] : m_Waypoints[0];
+                }
+                else
+                {
+                    if (m_WaypointIndex < m_Waypoints.Count)
+                    {
+                        m_TargetPosition = m_Waypoints[m_WaypointIndex];
+                    }
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, m_TargetPosition, 10f * Time.deltaTime);
+        }
+    }
+
 }
